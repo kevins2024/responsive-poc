28012c0b6cf53c9ea6d42636060679a04ee185c2
Kevin Smith <kevins2024@outlook.com>
2023-09-10 19:51:51 +0100

    two poc for react-based viewport size detection added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import "./app.css";
+// import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+
 function App() {
+  /* One approach to detecting window size.
+  const [width, setWidth] = useState(window.innerWidth);
+  console.log("Width: ", width);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+  **/
+  function breakpointMatcher(mediaQueryListObj) {
+    if (mediaQueryListObj.matches) {
+      console.log("small screen detected");
+    } else {
+      console.log("large screen detected");
+    }
+  };
+
+  useEffect(() => {
+    const mobileScreenRule = "(max-width: 480px)";
+    const mediaQueryList = window.matchMedia(mobileScreenRule);
+  
+    breakpointMatcher(mediaQueryList);
+
+    mediaQueryList.addEventListener("change", breakpointMatcher);
+
+    return () => mediaQueryList.removeEventListener("change", breakpointMatcher);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This page exists to test resizing of the viewport.
         </p>
       </header>
     </div>
